8ceb06494c36a791a2ce62b88fda12cba605c35f
piyushdhanore <piyushdhanore14@gmail.com>
2024-03-22 12:26:23 +0530

    JS Mini Project

---
+++ login.js
@@ -0,0 +1,16 @@
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+function fetchLoginData(){
+
+	let userCred = JSON.parse(localStorage.getItem("userData"));
+
+	if(((email.value)==(userCred.username))&&((password.value)==(userCred.password)))
+	{
+		open("Homepage2.html");
+	}
+	else
+	{
+		alert("WRONG CREDENTIALS");
+	}
+}
---
+++ reg.js
@@ -0,0 +1,35 @@
+let un = document.getElementById("email");
+let pass = document.getElementById("password");
+let add = document.getElementById("address");
+let gd = document.getElementById("graduation");
+let brnch = document.getElementById("branch");
+let yop = document.getElementById("yop");
+
+function registerUser(){
+	
+	let userData={
+		username: un.value,
+		password: pass.value,
+		address: add.value,
+		graduation: gd.value,
+		branch: brnch.value,
+		yop:yop.value
+	}
+
+	saveData(userData);
+	clearFeild();
+	open("Login.html"); 
+}
+
+function clearFeild() {
+	un.value="";
+	pass.value="";
+	add.value="";
+	gd.value="";
+	brnch.value="";
+	yop.value="";
+}
+
+function saveData(userData) {
+	localStorage.setItem("userInfo",userData);
+}
